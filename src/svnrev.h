e8d3c6ca766561e36b8cf5ea0f856283038b23b8
C.W. Betts <computers57@hotmail.com>
2021-01-24 20:12:31 -0700

    Add Apple Silicon support.

--- src/svnrev.h
+++ src/svnrev.h
@@ -1,5 +1,5 @@
 // REVISION TRACKING
 // This file is auto-generated.
 // Do not commit this file to the code repository!
-#define SVN_REV  
-#define SVN_REV_STR " "
+#define SVN_REV 0
+#define SVN_REV_STR "0"
